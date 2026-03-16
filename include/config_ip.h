45d2e28d59454071864972d1253214a7b801b5bb
Angyrflop <justanyordinarydude@gmail.com>
2026-03-16 23:33:47 +0100

    Added Magic number

--- include/config_ip.h
+++ include/config_ip.h
@@ -4,4 +4,6 @@
     #define START_CAPACITY  4
     #define BAN_FILE_PATH  "banned_ips.bin"
     #define WHITELISTED_FILE_PATH  "whitelisted_ips.bin"
+    #define MAGIC "\x23\x37\x21\x50\x6f"
+    #define MAGIC_LEN 5
 #endif  /*CONFIG_IP_H*/
