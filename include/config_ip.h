fa6653f1795ef005d52d462e34fac39c32d318ab
Angyrflop <justanyordinarydude@gmail.com>
2026-03-21 18:42:26 +0100

    Switching from Crow to libmicrohttpd. It was just causing too many problems.

--- include/config_ip.h
+++ include/config_ip.h
@@ -4,6 +4,6 @@
     #define MAX_CALLCOUNT  2000
     #define START_CAPACITY  4
     #define IP_FILE_PATH "ips.bin"
-    const uint64_t MAGIC = 0x0123456789ABCDEFULL;
+    static const uint64_t MAGIC = 0x0123456789ABCDEFULL;
 #endif  /*CONFIG_IP_H*/
 
